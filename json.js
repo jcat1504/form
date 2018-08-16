527e5cb969bd79e05f9bc4397dc01b35be71910a
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-08-16 16:31:40 -0700

    Update json.js
    Added some more JSON stuff.

--- json.js
+++ json.js
@@ -3,6 +3,11 @@ function submitForm() {
         task: myForm.task.value,
         description: myForm.description.value
     };
+    
+    myJSON = JSON.stringify(FormData);
+    localStorage.setItem("formJSON", myJSON);
+    text = localStorage.getItem("formJSON");
+    obj = JSON.parse(text);
 
     console.log(FormData);
     return false;
