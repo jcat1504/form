df16e96787cf424d595e0a86d5eb0e6382a24663
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-08-28 10:06:40 -0700

    Update json.js
    added comments for everything

--- json.js
+++ json.js
@@ -1,3 +1,5 @@
+//everything for submit button//
+
 function submitForm() {
     var task = myForm.task.value;
     var desc = myForm.description.value;
@@ -18,19 +20,24 @@ function submitForm() {
     return false;
 };
 
+
+//storing task and description in array
 newArray = [task, description];
 
 var taskArray = [];
 var descriptionArray = [];
 
+//creating variables task and description in order to save user's input, we use getElementById and get from input field
 var task = document.getElementById("task").value;
 var description = document.getElementById("description").value;
 
+//adding new input into task array. 
 function addTask(task) {
     taskArray.push(task);
     console.log("Tasks: " + taskArray.join(", "));
 }
 
+//adding new input into description array
 function addDescription(description) {
     descriptionArray.push(description);
     console.log("Description: " + descriptionArray.join(", "));
